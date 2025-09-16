77a0449c652091746e36915441cc30fb0bca1095
Abdelaziz <zez0m0ussa@yahoo.com>
2025-09-16 17:35:54 +0300

    first commit

--- dist/e-commerce/server/chunk-ZXY5HG4M.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{c as o,i}from"./chunk-MWDYRLYF.mjs";import{R as e,W as r}from"./chunk-6DECXFDM.mjs";var n=class t{httpClient=r(o);getAllCategories(){return this.httpClient.get(i.baseUrl+"categories")}static \u0275fac=function(a){return new(a||t)};static \u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"})};export{n as a};
